71a6fbeaa5518c61c0b98ddd69b239629da6fce8
PC <amolujagare@gmail.com>
2020-07-17 11:12:28 +0530

    constructors

--- src/ClassExample/ClassDemo.java
+++ src/ClassExample/ClassDemo.java
@@ -7,7 +7,47 @@ public class ClassDemo {
     String str;
     char c;
 
-    public void display()
+    ClassDemo() // empty contructors
+    {
+         // no body
+    }
+
+    /*ClassDemo() // normal constructors
+    {
+      i =10;
+      d= 1.1;
+      str="abcd";
+      c='s';
+    }*/
+
+   /* ClassDemo(int x,double y,String ss ,char cc) // parameterized constructor
+    {
+        i =x;
+        d=y;
+        str=ss;
+        c=cc;
+
+    }*/
+
+    ClassDemo(int i,double d,String str ,char c) // parameterized constructor with 'this'operator
+    {
+        this.i =i; // 'this' refers to current object
+        this.d=d;
+        this.str=str;
+        this.c=c;
+
+    }
+
+    ClassDemo(ClassDemo obj) // copy constructor
+    {
+        i=obj.i;
+        d=obj.d;
+        str=obj.str;
+        c=obj.c;
+
+    }
+
+    public void display() // function / method
     {
         System.out.println("i="+i);
         System.out.println("d="+d);
@@ -17,14 +57,13 @@ public class ClassDemo {
 
     public static void main(String[] args)
     {
-        ClassDemo ob = new ClassDemo();
 
-        ob.i=89;
-        ob.d=34.6;
-        ob.c= 'h';
-        ob.str= "xyz";
-
+        ClassDemo ob = new ClassDemo(12,23.5,"amol",'h'); // instance of the class
         ob.display();
+
+        ClassDemo ob1 = new ClassDemo();
+        ob1.display();
+
     }
 
 }
